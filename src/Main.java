702e132075802e8b2ae9aa6968a11e161253bc48
eminkilic <kilicemin404@gmail.com>
2026-01-29 21:51:02 +0100

    Opgave 1 færdig

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,22 @@ public class Main {
 void main (){
     //opgave 1
 
-    asdkasdkpasd
+    double vare1 = 25.50;
+
+    double vare2 = 15.00;
+
+    double vare3 = 30.00;
+
+    double subtotal = 25.50+15.00+30.00;
+
+    double momsProcent=0.25;
+
+    double samletPris = 70.50+(70.50*0.25);
+
+    System.out.print(subtotal+samletPris);
+
+
+
 }
 
 }
